0d2469d50c9e72ce1af6ade07bf51426778f9353
Egill Smári Snorrasons <egill98@hotmail.com>
2020-02-29 21:53:41 +0000

    fyrst push

--- minesweeper.js
+++ minesweeper.js
@@ -9,25 +9,49 @@ function generateBoard() {
             var cols = document.getElementById("columns").value;
             var mines = document.getElementById("mines").value;
 
+            if (rows > 40 || cols > 40 || rows < 1 || cols < 1){
+                rows = 10;
+                cols = 10;
+            }
+            if (mines > rows*cols){
+                mines = 10;
+            }
+
             //Perform an AJAX POST request to the url, and set the param 'myParam' in the request body to paramValue
             axios.post(url, { rows: rows, cols: cols, mines: mines })
                 .then(function (response) {
                     //When successful, print 'Success: ' and the received data
                     console.log("Success: ", response.data.board);
 
+
                     // Generating the board
+                    // create bomb position array
+                    var minesArray = response.data.board.minePositions;
+                    var board_array = [];
                     for (i=0;i<cols;i++) {
+                        board_array[i] = [];
                         for (j=0;j<rows;j++){
                             var btn = document.createElement("BUTTON");
                             btn.id = "" + i + "-" + j;
                             btn.className = "field";
-                            btn.style.height = "20px";
-                            btn.style.width = "20px";
+                            btn.style.height = "40px";
+                            btn.style.width = "40px";
+                            if (isMine(i, j, minesArray) === true){
+                                board_array[i][j] = "M";
+                                btn.innerText = "M"
+                            }
+                            else{
+                                board_array[i][j] = [i,j];
+                                btn.innerText = "" + i + "-" + j
+                            }
                             document.getElementById("game-board").appendChild(btn);
                         }
                         var brk = document.createElement("br");
                         document.getElementById("game-board").appendChild(brk);
                     }
+                    console.log(board_array);
+                    placeNumbers(board_array, cols, rows);
+
 
 
                     //
@@ -49,19 +73,7 @@ function generateBoard() {
 
 
 
-                    // Making a matrix for the board.
-                    var board_array = [];
-                    for (var i=0;i<rows;i++) {
-                    board_array[i] = [];
-                    }
-                    for (i=0;i<rows;i++){
-                        for (j=0;j<cols;j++){
-                            board_array[i][j] = 0
-                        }
-                    }
-                    console.log(board_array)
-
-
+                    // Making a matrix for the board
 
                 })
 
@@ -73,3 +85,93 @@ function generateBoard() {
                     // This code is always executed, independent of whether the request succeeds or fails.
                 });
         }
+
+function isMine(rows, cols, minesArray) {
+    for (var x = 0; x < minesArray.length; x++) {
+        var mine = minesArray[x];
+
+        if (mine[0] === rows && mine[1] === cols){
+            return true
+        }
+            }
+}
+
+function placeNumbers(board_array, cols, rows) {
+    for (i=0;i<cols;i++){
+        for (j=0;j<rows;j++) {
+            var temp = board_array[i][j];
+            console.log(board_array[i]);
+
+            if (board_array[i][j] === "M") {
+                console.log("bomb!!!!");
+                continue
+            }
+            try{
+            if (board_array[i-1] [j-1] === "M"){
+                board_array[i][j].innerHTML += 1
+            }
+            }
+            catch (e) {}
+            try{
+            if (board_array[i][j-1] === "M"){
+                board_array[i][j].innerHTML += 1
+            }
+            }
+            catch (e) {}
+            try{
+            if (board_array[i][j+1] === "M"){
+                board_array[i][j].innerHTML += 1
+            }
+            }
+            catch (e) {}
+            try{
+            if (board_array[i+1][j-1] === "M"){
+                board_array[i][j].innerHTML += 1
+            }
+            }
+            catch (e) {}
+            try{
+                if (board_array[i+1][j+1] === "M"){
+                    board_array[i][j].innerHTML += 1
+            }
+            }
+            catch (e) {}
+            try{
+                if (board_array[i+1][j] === "M"){
+                    board_array[i][j].innerHTML += 1
+            }
+            }
+            catch (e) {}
+            try {
+                if (board_array[i - 1][j + 1] === "M") {
+                    board_array[i][j].innerHTML += 1
+                }
+            }
+            catch (e) {}
+            }
+            }
+    console.log(board_array);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
