3bfc666a8bde9afa169ee7a24a154e29c44251dc
Egill Smári Snorrasons <egill98@hotmail.com>
2020-03-01 19:19:21 +0000

    fyrst push

--- minesweeper.js
+++ minesweeper.js
@@ -1,6 +1,6 @@
+var BOMBFLAGGED = 0;
+var PRESSEDMINES = 0;
 function generateBoard() {
-            //Prepare the parameter value for 'myParam'
-            var paramValue = "someValue";
 
             //The URL to which we will send the request
             var url = 'https://veff213-minesweeper.herokuapp.com/api/v1/minesweeper';
@@ -23,58 +23,9 @@ function generateBoard() {
                     //When successful, print 'Success: ' and the received data
                     console.log("Success: ", response.data.board);
 
-
-                    // Generating the board
-                    // create bomb position array
-                    var minesArray = response.data.board.minePositions;
-                    var board_array = [];
-                    for (i=0;i<cols;i++) {
-                        board_array[i] = [];
-                        for (j=0;j<rows;j++){
-                            var btn = document.createElement("BUTTON");
-                            btn.id = "" + i + "-" + j;
-                            btn.className = "field";
-                            btn.style.height = "40px";
-                            btn.style.width = "40px";
-                            if (isMine(i, j, minesArray) === true){
-                                board_array[i][j] = "M";
-                                btn.innerText = "M"
-                            }
-                            else{
-                                board_array[i][j] = [i,j];
-                                btn.innerText = "" + i + "-" + j
-                            }
-                            document.getElementById("game-board").appendChild(btn);
-                        }
-                        var brk = document.createElement("br");
-                        document.getElementById("game-board").appendChild(brk);
-                    }
-                    console.log(board_array);
-                    placeNumbers(board_array, cols, rows);
-
-
-
-                    //
-                    for (i=0;i<cols;i++){
-                        for (j=0;j<rows;j++){
-                            document.getElementById("" + i + "-" + j).onmousedown = function (event) {
-                                if (event.which === 1){
-                                    alert("what")
-                                }
-                                else if (event.which === 3){
-                                    var red_flag = document.createElement("img");
-                                    red_flag.id = "flag";
-                                    red_flag.src = "images/flag.png";
-                                    document.getElementById(this.id).appendChild(red_flag);
-                                }
-                            }
-                        }
-                    }
-
-
-
-                    // Making a matrix for the board
-
+                    var board_array = createMineSweeper(rows, cols, response);
+                    addEvents(board_array, rows, cols, mines);
+                    console.log(board_array)
                 })
 
                 .catch(function (error) {
@@ -85,6 +36,102 @@ function generateBoard() {
                     // This code is always executed, independent of whether the request succeeds or fails.
                 });
         }
+
+
+function addEvents(board_array, rows, cols, mines) {
+    for (let i=0;i<cols;i++){
+        for (let j=0;j<rows;j++){
+            document.getElementById("" + i + "-" + j).onmousedown = function (event) {
+            if (event.which === 1){
+                console.log(board_array[i][j]);
+                document.getElementById(this.id).innerHTML = board_array[i][j];
+                board_array[i][j] = ""+board_array[i][j]+"P";
+                console.log(board_array);
+                PRESSEDMINES += 1;
+                isWinner(mines, board_array);
+                isLooser(board_array[i][j])
+
+            }
+            else if (event.which === 3){
+
+                var red_flag = document.createElement("img");
+                red_flag.id = "flag";
+                red_flag.src = "images/flag.png";
+                document.getElementById(this.id).appendChild(red_flag);
+                if (board_array[i][j] === "M"){
+                    BOMBFLAGGED+= 1;
+                    isWinner(mines, rows, cols);
+                    console.log("dis da bomb");
+
+                }
+                board_array[i][j] = ""+board_array[i][j]+"F";
+
+
+            }
+        }
+    }
+}
+    return board_array
+}
+
+function isWinner(mines, rows, cols) {
+    rows = parseInt(rows);
+    mines = parseInt(mines);
+    cols = parseInt(cols);
+    if (BOMBFLAGGED === mines && PRESSEDMINES === rows*cols - mines){
+        alert("YOU JUST WON THE GAME MATE");
+        killGame()
+    }
+}
+
+function isLooser(currentPressed) {
+    console.log(currentPressed);
+    if(currentPressed === "MP"){
+        alert("YOU JUST LOST THE GAME MATE");
+        killGame()
+    }
+    
+}
+
+function killGame(){
+    // implement function that stops all process and you can only press the generate board button
+}
+
+function createMineSweeper(rows, cols, response) {
+    cleanBoard();
+    var minesArray = response.data.board.minePositions;
+        var board_array = [];
+        for (i=0;i<cols;i++) {
+            board_array[i] = [];
+            for (j=0;j<rows;j++){
+                var btn = document.createElement("BUTTON");
+                btn.id = "" + i + "-" + j;
+                btn.className = "field";
+                btn.style.height = "25px";
+                btn.style.width = "25px";
+                if (isMine(i, j, minesArray) === true){
+                    board_array[i][j] = "M";
+                }
+                else{
+                    board_array[i][j] = 0;
+                }
+                document.getElementById("game-board").appendChild(btn);
+            }
+            var brk = document.createElement("br");
+            document.getElementById("game-board").appendChild(brk);
+        }
+        placeNumbers(board_array, cols, rows);
+        return board_array
+
+}
+
+function cleanBoard() {
+    var board = document.getElementById("game-board");
+    board.innerHTML = "";
+
+
+}
+
 
 function isMine(rows, cols, minesArray) {
     for (var x = 0; x < minesArray.length; x++) {
@@ -103,10 +150,7 @@ function placeNumbers(board_array, cols, rows) {
         for (j=0;j<rows;j++) {
             tempLis[i].push(0);
             var tempBomb = 0;
-            console.log(board_array[i]);
-            var btn = document.getElementById("" + i + "-" + j);
             if (board_array[i][j] === "M") {
-                console.log("bomb!!!!");
                 tempLis[i][j] = "M";
                 continue
             }
@@ -151,31 +195,8 @@ function placeNumbers(board_array, cols, rows) {
                     tempBomb += 1}
             }
             catch (e) {}
-            btn.innerHTML = ""+tempBomb
+            board_array[i][j] = JSON.stringify(tempBomb);
             }
     }
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
